bc595e2c19c7974e050c41f78ca5c4a9098d0404
unknown <nikhil.salvi@intellection.in>
2021-08-20 10:46:24 +0530

    Nikhil_Django_Project

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('meetups',views.index),
-    path('meetups/<slug:meetup_slug>',views.meetup_details), #dynamic path
+    path('meetups/<slug:meetup_slug>',views.meetup_details), 
 ]
